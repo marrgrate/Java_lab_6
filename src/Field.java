868fcbc1c5c9a446d5b49b13318692358194d6e3
marrygran <marrygran28@gmail.com>
2021-02-22 17:29:13 +0300

    Exception in thread AWT-EventQueue-0 : 32

--- src/Field.java
+++ src/Field.java
@@ -1,18 +1,53 @@
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class Field extends JPanel {
-    private ArrayList<BouncingBall> balls = new ArrayList<BouncingBal>(10);
+
+    private ArrayList<BouncingBall> balls = new ArrayList<BouncingBall>(10);
     private boolean paused;
 
-    private Timer repaintTimer = new Timer(10), new ActionListener(){
-        public void actionPerformed(ActionEvent ev){
-            repaint();
-        }
-    }
+    private Timer repaintTimer = new Timer(10,
+            new ActionListener(){
+                 public void actionPerformed(ActionEvent ev){
+                     repaint();
+                 }
+            });
 
     public Field(){
         setBackground(Color.BLACK);
+        repaintTimer.start();
+    }
+
+    public void addBall(){
+        balls.add(new BouncingBall(this));
+    }
+
+    public void paintComponent(Graphics gr) {
+        super.paintComponent(gr);
+        Graphics2D canvas = (Graphics2D) gr;
+        for (BouncingBall ball: balls) {
+            ball.paint(canvas);
+        }
+    }
+
+    public synchronized void pause() {
+        paused = true;
+    }
+    public synchronized void resume() {
+        paused = false;
+        notifyAll();
+    }
+    public synchronized void canMove(BouncingBall ball)
+            throws InterruptedException{
+        if(paused) {
+            wait();
+        }
     }
 
 }
